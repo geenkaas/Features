b51670c0d1eb2add5264691fde621bedc2028c97
Firstname LastnameGeen <github@geenkaas.nl>
2012-07-24 11:16:58 +0200

    Added facebook wallpost

--- Facebook wallpost/index.php
+++ Facebook wallpost/index.php
@@ -1,25 +1,27 @@
 
 	<div id="fbsend">
-		<div class="fbfeed pie" id="fb-login">
-			<a onclick='javascript:loginFB();' onlogin="Log.info('onlogin callback')">Deel dit op Facebook</a>
-		</div>
+		<a onclick='javascript:loginFB();' onlogin="Log.info('onlogin callback')">
+			Deel dit op Facebook
+		</a>
 	</div>
 
-	<script>
-		window.fbAsyncInit = function() {
-			FB.init({
-				appId 	: '266805616685359',
-				status	: true, //	check login status
-				cookie	: true, //	enable cookies to allow the server to access the session
-				xfbml	: true,	//	parse XFBML,
-				oauth	: true	//	Open Auth 2.0 since december 13th
-			});
-		};
-		(function() {
-			var e = document.createElement('script');
-			e.src = document.location.protocol + '//connect.facebook.net/nl_NL/all.js';
-			e.async = true;
-			document.getElementById('fb-root').appendChild(e);
-		}());
-	</script>
+	<!-- Facebook -->
+		<div id="fb-root"></div>
+		<script>
+			window.fbAsyncInit = function() {
+				FB.init({
+					appId 	: 'REPLACE' //	https://developers.facebook.com/apps
+					status	: true,
+					cookie	: true,
+					xfbml	: true,
+					oauth	: true
+				});
+			};
+			(function() {
+				var e = document.createElement('script');
+				e.src = document.location.protocol + '//connect.facebook.net/nl_NL/all.js';
+				e.async = true;
+				document.getElementById('fb-root').appendChild(e);
+			}());
+		</script>
 
--- Facebook wallpost/script.js
+++ Facebook wallpost/script.js
@@ -1,3 +1,4 @@
+//	Facebook Wallpost
 
 	function loginFB() {
 		FB.login(function(response) {	// user is logged in
@@ -13,35 +14,26 @@
 			scope:'publish_stream,email'
 		});
 	}
+
 	
 	var wallpost = {
-		keuze1: '',
-		keuze2: '',
-		keuze3: '',
-		wallPost:	'',
 		init: function() {
-			wallpost.keuze1 = $('#top3selection div.slot1').attr('keuze');
-			wallpost.keuze2 = $('#top3selection div.slot2').attr('keuze');
-			wallpost.keuze3 = $('#top3selection div.slot3').attr('keuze');
-			wallpost.keuze1img = $('#top3selection div.slot1').attr('excerpt');
 		},
 		postToWall: function(fbid) {
 			wallpost.wallPost = {
-				message:		'',
-				link:			'http://www.fairminds.nl/',
-				name:			'Fairminds',
-				picture:		'http://www.fairminds.nl/wp-content/themes/bandbreed/images/producten/product_'+ wallpost.keuze1img +'.jpg',
-				message:		'Hee! Dit is mijn fairlanglijstje voor de feestdagen. Ben benieuwd naar die van jou. Maak, deel en win jouw fairlanglijstje op www.fairminds.nl.',
-				caption:		'Dit is mijn top3.',
-				description:	'Keuze 1: '+ wallpost.keuze1 +' - Keuze 2: '+ wallpost.keuze2 +' - Keuze 3: '+ wallpost.keuze3 +''
+				link:			'http://www.link.nl/',
+				name:			'dit is de name',
+				picture:		'http://client.bandbreed.nl/enexis/images/logo_enexis.png',
+				message:		'dit is de message',
+				caption:		'dit is de caption',
+				description:	'dit is de description'
 			}
 			FB.api('/'+fbid+'/feed', 'post', wallpost.wallPost, function(response) {
 				if (!response || response.error) {
-					alert('Er is iets mis gegaan met het posten op je wall... sorry :(');
+					alert('Er is iets mis gegaan met het posten op je wall... excuus.');
 				} else {
-					window.location = '/save/?keuze1='+wallpost.keuze1+'&keuze2='+wallpost.keuze2+'&keuze3='+wallpost.keuze3;
+					// window.location = '/save/';
 				}
 			});
 		}
 	}
-	
